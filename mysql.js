23d7f6cd596e1de27393868a604fd727bf0cc6ae
Jonatha Lima <jonatha.lima14@gmail.com>
2017-01-03 16:39:54 -0300

    added functions to update, select and delete rows.

--- mysql.js
+++ mysql.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 	password: ""
 });
 
-var LIMIT = 100;
+var LIMIT = 1;
 
 function createBD(callback){
 	if (connection) {
@@ -23,7 +23,7 @@ function createBD(callback){
 }
 
 function createTable(callback) {
-	var truncate = "TRUNCATE `bd_performance_test.user`";
+	var truncate = "TRUNCATE bd_performance_test.user";
 	var table = "CREATE TABLE IF NOT EXISTS bd_performance_test.user (`id` INT NOT NULL auto_increment, `name` VARCHAR(255) ,`email` VARCHAR(255), `password` VARCHAR(255), PRIMARY KEY (`id`))";
 	
 	connection.query(table, function(err, results) {
@@ -58,12 +58,85 @@ function insert(callback) {
 	execute(callback);
 }
 
+function update(callback) {
+	var done = 0;
+
+	var execute = function (cb) {
+		var name = faker.name.findName();
+		var sql = "UPDATE bd_performance_test.user SET name = ? WHERE id = ?";
+
+		connection.query(sql, [name, done+1], function(err, results) {
+			if(err) throw new Error(err);
+			done++;
+			if (done === LIMIT) {
+				cb();
+			} else {
+				execute(cb);
+			}
+		});
+	}
+
+	console.time('mysql update');
+	execute(callback);
+}
+
+function select(callback) {
+	var done = 0;
+
+	var execute = function (cb) {
+		var sql = "SELECT * FROM bd_performance_test.user WHERE id = ?";
+
+		connection.query(sql, [done+1], function(err, results) {
+			if(err) throw new Error(err);
+			done++;
+			if (done === LIMIT) {
+				cb();
+			} else {
+				execute(cb);
+			}
+		});
+	}
+
+	console.time('mysql select');
+	execute(callback);
+}
+
+function deleteRows(callback) {
+	var done = 0;
+
+	var execute = function (cb) {
+		var sql = "DELETE FROM bd_performance_test.user WHERE id = ?";
+
+		connection.query(sql, [done+1], function(err, results) {
+			if(err) throw new Error(err);
+			done++;
+			if (done === LIMIT) {
+				cb();
+			} else {
+				execute(cb);
+			}
+		});
+	}
+
+	console.time('mysql delete');
+	execute(callback);
+}
+
 function main() {
 	createBD(function(){
 		createTable(function(){
 			insert(function(){
 				console.timeEnd('mysql insert');
-				process.exit(1);
+				update(function() {
+					console.timeEnd('mysql update');
+					select(function() {
+						console.timeEnd('mysql select')
+						deleteRows(function(){
+							console.timeEnd('mysql delete');
+							process.exit(1);
+						})
+					})
+				});
 			});
 		})
 	})
